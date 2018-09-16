f43e51bdbb5cff2e0c6f381b4e13202726c5d520
William <william.antonio@zup.com.br>
2018-09-15 23:01:18 -0300

    inicio

---
+++ js/principal.js
@@ -0,0 +1,26 @@
+// console.log("aeeee")
+// console.log(document.querySelector("h1")); 
+var titulo = document.querySelector(".titulo");
+// console.log(titulo);
+// console.log(titulo.textContent);
+titulo.textContent = "Aparecidade Sumiu";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdpeso = paciente.querySelector(".info-peso");
+var tdaltura = paciente.querySelector(".info-altura");
+
+
+var peso = tdpeso.textContent;
+var altura = tdaltura.textContent;
+
+var imc = peso/(altura*altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+
+tdImc.textContent = imc;
+
+console.log(paciente); // tr
+console.log(tdpeso); //td que contem peso
+console.log(peso);
+console.log(altura);
