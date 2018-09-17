c7eccb3419c506ed0162fa45b422fd1752dce52d
William <william.antonio@zup.com.br>
2018-09-17 12:22:57 -0300

    event target remove add

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,18 @@
 
-var pacientes = document.querySelectorAll(".paciente");
+// var pacientes = document.querySelectorAll(".paciente");
+// pacientes.forEach(function (paciente) {
+//     paciente.addEventListener("dblclick", function () {
+//         console.log("fui clicado com double click!");
+//         this.remove();
+//     });
+// });
 
-pacientes.forEach(function (paciente) {
-    paciente.addEventListener("dblclick", function () {
-        console.log("fui clicado com double click!");
-        this.remove();
-    });
+var table = document.querySelector("table");
+
+table.addEventListener("dblclick", function(event){
+    console.log(event.target);
+    var alvoEvento = event.target;
+    var paiDoEvento = alvoEvento.parentNode;
+
+    paiDoEvento.remove();
 });
